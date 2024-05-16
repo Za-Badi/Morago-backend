cd728eb76c910da41cd24270840076a8a5c77e58
Kate Kim <ekkim94@gmail.com>
2024-05-16 18:59:03 +0900

    Initial commit

---
+++ src/main/java/com/habsida/morago/MoragoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.habsida.morago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoragoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoragoBackendApplication.class, args);
+    }
+
+}
