3e12a57bce57c841b0eb571113aa02ac86869183
ZA-Badi <badi.zaha@gmail.com>
2024-05-29 19:00:38 +0200

    graphql configuration

--- src/main/java/com/habsida/morago/config/GraphQLConfiguration.java
+++ src/main/java/com/habsida/morago/config/GraphQLConfiguration.java
@@ -1,23 +1,2 @@
 //package com.habsida.morago.config;
 //
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.boot.autoconfigure.graphql.GraphQlSourceBuilderCustomizer;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration(proxyBeanMethods = false)
-//class GraphQLConfiguration {
-//
-//    private static Logger logger = LoggerFactory.getLogger("graphql");
-//
-//    @Bean
-//    public GraphQlSourceBuilderCustomizer sourceBuilderCustomizer() {
-//        return (builder) ->
-//                builder.
-//
-//                        inspectSchemaMappings(report -> {
-//                    logger.debug(report.toString());
-//                });
-//    }
-//}
