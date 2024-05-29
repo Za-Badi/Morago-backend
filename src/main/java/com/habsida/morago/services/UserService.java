5acdad2c33629a61c116a1bd0ad1c30e8f7f94ff
RixioMMT <rixio1994@gmail.com>
2024-05-28 22:43:46 -0400

    Week 3: GraphQL configuration

--- src/main/java/com/habsida/morago/services/UserService.java
+++ src/main/java/com/habsida/morago/services/UserService.java
@@ -1,32 +1,14 @@
 package com.habsida.morago.services;
 
 import com.habsida.morago.model.entity.User;
-import com.habsida.morago.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-@Service
-public class UserService {
-    private final UserRepository userRepository;
-
-    @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-    }
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    public List<User> allUsers() {
-        List<User> users = new ArrayList<>();
-        userRepository.findAll().forEach(users::add);
-        return users;
-    }
-    public User saveUser(User user) {
-        return userRepository.save(user);
-    }
+public interface UserService {
+    public List<User> getAllUsers();
+    public Optional<User> getUserById(String id) throws Exception;
+    public User addUser(User user);
+    public User updateUser(String id, User userUpdate) throws Exception;
+    public void deleteUser(String id) throws Exception;
 }
