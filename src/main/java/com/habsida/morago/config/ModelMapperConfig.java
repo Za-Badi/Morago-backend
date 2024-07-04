59cef62bba81859732f39887a669734cd8bfbae0
shatlykgurdov <148002323+shatlykgurdov@users.noreply.github.com>
2024-07-04 18:31:19 +0500

    model mapper last
    model mapper last

--- src/main/java/com/habsida/morago/config/ModelMapperConfig.java
+++ src/main/java/com/habsida/morago/config/ModelMapperConfig.java
@@ -1,6 +1,9 @@
 package com.habsida.morago.config;
 
+import com.habsida.morago.model.dto.DepositsDTO;
+import com.habsida.morago.model.entity.Deposits;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +12,17 @@ public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+
+        // Add custom mappings here
+        modelMapper.addMappings(new PropertyMap<Deposits, DepositsDTO>() {
+            @Override
+            protected void configure() {
+                // Explicitly map the PaymentStatus enum
+                map().setStatus(source.getStatus());
+            }
+        });
+
+        return modelMapper;
     }
 }
