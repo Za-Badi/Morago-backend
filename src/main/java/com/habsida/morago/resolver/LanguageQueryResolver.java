31d8353829a59e7fa9a6babd443a6ea9aaf73006
shatlykgurdov <148002323+shatlykgurdov@users.noreply.github.com>
2024-07-30 10:10:16 +0500

    language_service
    language_service

--- src/main/java/com/habsida/morago/resolver/LanguageQueryResolver.java
+++ src/main/java/com/habsida/morago/resolver/LanguageQueryResolver.java
@@ -3,19 +3,19 @@ package com.habsida.morago.resolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.habsida.morago.exceptions.ExceptionGraphql;
 import com.habsida.morago.model.dto.LanguageDTO;
+import com.habsida.morago.model.inputs.PagingInput;
+import com.habsida.morago.model.results.LanguagePageOutput;
 import com.habsida.morago.serviceImpl.LanguageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class LanguageQueryResolver implements GraphQLQueryResolver {
     private final LanguageService languageService;
 
-    public List<LanguageDTO> getAllLanguages() {
-        return languageService.getAllLanguages();
+    public LanguagePageOutput getAllLanguages(PagingInput pagingInput) {
+        return languageService.getAllLanguages(pagingInput);
     }
 
     public LanguageDTO getLanguageById(Long id) throws ExceptionGraphql {
